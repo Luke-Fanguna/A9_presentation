bf3b7ccdaf9a83ac698c65c0497bf1bbed61f07b
rjsal <rjsalaz888@gmail.com>
2024-03-14 15:46:34 -0700

    added ifC

--- A9.go
+++ A9.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-    "strconv"
-    //"os"
+	"strconv"
+	//"os"
 )
 
 type ExprC interface {
-    init() string
+	init() string
 }
 
 type numC struct {
@@ -22,33 +22,48 @@ type strC struct {
 	s string
 }
 
+type ifC struct {
+	cond ExprC
+	then ExprC
+	els  ExprC
+}
+
 func (num numC) init() string {
-    return strconv.Itoa(num.n)
+	return strconv.Itoa(num.n)
 }
 
 func (exp idC) init() string {
-    return exp.id
+	return exp.id
 }
 
 func (exp strC) init() string {
-    return exp.s
+	return exp.s
 }
 
-func interp (e ExprC) string {
-    switch v := e.(type){
-    case numC:
-        return strconv.Itoa(v.n)
-    case idC:
-        return v.id
-    case strC:
-        return v.s
-    default:
-        return "unimplemented"
-    }
+func (exp ifC) init() string {
+	return interp(exp.cond)
+}
+
+func interp(e ExprC) string {
+	switch v := e.(type) {
+	case numC:
+		return strconv.Itoa(v.n)
+	case idC:
+		return v.id
+	case strC:
+		return v.s
+	case ifC:
+		if interp(v.cond) == "true" {
+			return interp(v.then)
+		} else {
+			return interp(v.els)
+		}
+	default:
+		return "unimplemented"
+	}
 }
 
 // possibly add i/o with os import?
 func main() {
-	fmt.Println(interp(numC{5}));
+	fmt.Println(interp(numC{5}))
 }
-
--- A9_test.go
+++ A9_test.go
@@ -1,27 +1,49 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestInterp(t *testing.T) {
-    t.Run("numC", func(t *testing.T) {
-        tst := numC{
-            n:4,
-        }
-        exp := interp(tst)
-        if exp != "4" {
-            t.Fatalf("Got: %v\n", exp)
-        }
-    })
+	t.Run("numC", func(t *testing.T) {
+		tst := numC{
+			n: 4,
+		}
+		exp := interp(tst)
+		if exp != "4" {
+			t.Fatalf("Got: %v\n", exp)
+		}
+	})
 
-    t.Run("idC", func(t *testing.T) {
-        tst := idC{
-            id:"hello",
-        }
-        exp := interp(tst)
-        if exp != "hello" {
-            t.Fatalf("Got: %v\n", exp)
-        }
-    })
+	t.Run("idC", func(t *testing.T) {
+		tst := idC{
+			id: "hello",
+		}
+		exp := interp(tst)
+		if exp != "hello" {
+			t.Fatalf("Got: %v\n", exp)
+		}
+	})
+
+	t.Run("strC", func(t *testing.T) {
+		tst := strC{
+			s: "hello there",
+		}
+		exp := interp(tst)
+		if exp != "hello there" {
+			t.Fatalf("Got: %v\n", exp)
+		}
+	})
+
+	t.Run("ifC", func(t *testing.T) {
+		tst := ifC{
+			cond: idC{"true"},
+			then: numC{1},
+			els:  numC{2},
+		}
+		exp := interp(tst)
+		if exp != "1" {
+			t.Fatalf("Got: %v\n", exp)
+		}
+	})
 }
